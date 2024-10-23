7c9197a18356c2101a002a84856eaca0f028b1b5
Jyothi <146377527+Jyotheekiran@users.noreply.github.com>
2024-10-23 12:28:28 +0100

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-var http = require("http");
-const port = 8000;
-
-http.createServer(function(req, res) {
-    res.writeHead(200, { "Content-Type": "text/html" }); // Ensure Content-Type is text/html
-    res.end("<h1>Hello World!</h1>\n<p>I am Jyothi and welcome Dynamic Web Applications</p>"); // HTML content
-}).listen(port, function() {
-    console.log(`Node server is running on port ${port}...`);
-});
